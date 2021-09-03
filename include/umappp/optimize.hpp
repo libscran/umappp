f35fb76c14a1f4164370db72b3475e2024eb9265
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-09-02 23:03:31 -0700

    Fixes to the optimizer for compilation to work. Removed race condition-inducing parallelization.

--- include/umappp/optimize.hpp
+++ include/umappp/optimize.hpp
@@ -10,14 +10,13 @@
 
 namespace umappp {
 
-template<class Prob, class Gradient>
-void optimize_layout(
+inline void optimize_layout(
     int num_dim,
-    size_t num_obs, 
     double* embedding, 
-    const Prob& probabilities, 
-    int num_epochs,
+    const std::vector<size_t>& head, 
+    const std::vector<int>& tail, 
     const std::vector<double>& epochs_per_sample,
+    int num_epochs,
     double a, 
     double b, 
     double gamma,
@@ -25,92 +24,87 @@ void optimize_layout(
     double negative_sample_rate
 ) {
     constexpr double dist_eps = std::numeric_limits<double>::epsilon();
-    std::vector<uint_fast64_t> seeds(num_obs);
     std::mt19937_64 overlord(123456790);
+    const size_t num_obs = head.size(); 
 
     // Defining epoch-related constants.
-    std::vector<double> epochs_per_next_sample(epochs_per_sample); 
-    std::vector<double> epochs_per_negative_sample(epochs_per_sample);
-    for (auto& e : epochs_per_negative_sample) {
+    std::vector<double> epoch_of_next_sample(epochs_per_sample); 
+    std::vector<double> epoch_of_next_negative_sample(epochs_per_sample);
+    for (auto& e : epoch_of_next_negative_sample) {
         e /= negative_sample_rate;
     }
-    std::vector<double> epochs_per_next_negative_sample(epochs_per_negative_sample);
 
     // Defining the gradient parameters.
     constexpr double min_gradient = -4;
     constexpr double max_gradient = 4;
 
     for (int n = 0; n < num_epochs; ++n) {
-        const double alpha = initial_alpha * (1.0 - static_cast<double>(n) / n_epochs);
+        const double alpha = initial_alpha * (1.0 - static_cast<double>(n) / num_epochs);
 
-        // Generating seeds for each observation. This ensures that we get the
-        // same results regardless of the number of threads.
-        for (auto& x : seeds) {
-            x = overlord();
-        }
+        for (size_t i = 0; i < head.size(); ++i) {
+            size_t start = (i == 0 ? 0 : head[i-1]), end = head[i];
+            double* left = embedding + i * num_dim;
 
-        #pragma omp parallel
-        {
-            #pragma omp for
-            for (size_t i = 0; i < num_obs; ++i) {
-                if (epoch_of_next_sample[i] > n) {
+            for (size_t j = start; j < end; ++j) {
+                if (epoch_of_next_sample[j] > n) {
                     continue;
                 }
-                std::mt19937_64 rng(seeds[i]);
 
-                for (const auto& neighbor : probabilities[i]) {
-                    double dist2 = 0;
-                    {
-                        const double* left = embedding + i * num_dim;
-                        const double* right = embedding.data() + neighbor.first * num_dim;
-                        for (int d = 0; d < num_dim; ++d, ++left, ++right) {
-                            dist2 += (*left - *right) * (*left - *right);
-                        }
-                        ist2 = std::max(dist_eps, dist2);
+                double dist2 = 0;
+                double* right = embedding + tail[j] * num_dim;
+                {
+                    const double* lcopy = left;
+                    const double* rcopy = right;
+                    for (int d = 0; d < num_dim; ++d, ++lcopy, ++rcopy) {
+                        dist2 += (*lcopy - *rcopy) * (*lcopy - *rcopy);
                     }
-
-                    const double pd2b = std::pow(dist2, b);
-                    const double grad_coef = (-2 * a * b * pd2b) / (dist2 * (a * pd2b + 1.0));
-                    {
-                        double* left = embedding + i * num_dim;
-                        double* right = embedding + neighbor.first * num_dim;
-                        for (int d = 0; d < num_dim; ++d, ++left, ++right) {
-                            double gradient = alpha * std::min(std::max(grad_coef * (*left - *right), min_gradient), max_gradient);
-                            *left += gradient;
-                            *right -= gradient;
-                        }
-                    }
-
-                    const size_t num_neg_samples = (n - epoch_of_next_negative_sample[i]) / epochs_per_negative_sample[i];
-                    for (size_t p = 0; p < num_neg_samples; ++p) {
-                        size_t sampled = (rng() % num_obs) * num_dim; // TODO: fix the sampler
-                        if (sampled == i) {
-                            continue;
-                        }
-
-                        double dist2 = 0;
-                        {
-                            const double* left = embedding + i * num_dim;
-                            const double* right = embedding + sampled * num_dim;
-                            for (int d = 0; d < num_dim; ++d, ++left, ++right) {
-                                dist2 += (*left - *right) * (*left - *right);
-                            }
-                            dist2 = std::max(dist_eps, dist2);
-                        }
-
-                        const double grad_coef = 2 * gamma * b / ((0.001 + dist2) * (a * std::pow(dist2, b) + 1.0));
-                        {
-                            double* left = embedding + i * num_dim;
-                            double* right = embedding + sampled * num_dim;
-                            for (int d = 0; d < num_dim; ++d, ++left, ++right) {
-                                *left += alpha * std::min(std::max(grad_coef * (*left - *right), min_gradient), max_gradient);
-                            }
-                        }
-                    }
-
-                    epoch_of_next_sample[i] += epochs_per_sample[i];
-                    epoch_of_next_negative_sample[i] += num_neg_samples * epochs_per_negative_sample[i];
+                    dist2 = std::max(dist_eps, dist2);
                 }
+
+                const double pd2b = std::pow(dist2, b);
+                const double grad_coef = (-2 * a * b * pd2b) / (dist2 * (a * pd2b + 1.0));
+                {
+                    double* lcopy = left;
+                    double* rcopy = right;
+                    for (int d = 0; d < num_dim; ++d, ++lcopy, ++rcopy) {
+                        double gradient = alpha * std::min(std::max(grad_coef * (*lcopy - *rcopy), min_gradient), max_gradient);
+                        *lcopy += gradient;
+                        *rcopy -= gradient;
+                    }
+                }
+
+                // const double epochs_per_negative_sample = epochs_per_sample[j] / negative_sample_rate;
+                const size_t num_neg_samples = (n - epoch_of_next_negative_sample[j]) * (negative_sample_rate / epochs_per_sample[i]);
+
+                for (size_t p = 0; p < num_neg_samples; ++p) {
+                    size_t sampled = (overlord() % num_obs) * num_dim; // TODO: fix the sampler
+                    if (sampled == i) {
+                        continue;
+                    }
+
+                    double dist2 = 0;
+                    double* right = embedding + sampled * num_dim;
+                    {
+                        const double* lcopy = left;
+                        const double* rcopy = right;
+                        for (int d = 0; d < num_dim; ++d, ++lcopy, ++rcopy) {
+                            dist2 += (*lcopy - *rcopy) * (*lcopy - *rcopy);
+                        }
+                        dist2 = std::max(dist_eps, dist2);
+                    }
+
+                    const double grad_coef = 2 * gamma * b / ((0.001 + dist2) * (a * std::pow(dist2, b) + 1.0));
+                    {
+                        double* lcopy = left;
+                        const double* rcopy = right;
+                        for (int d = 0; d < num_dim; ++d, ++lcopy, ++rcopy) {
+                            *lcopy += alpha * std::min(std::max(grad_coef * (*lcopy - *rcopy), min_gradient), max_gradient);
+                        }
+                    }
+                }
+
+                epoch_of_next_sample[j] += epochs_per_sample[j];
+                epoch_of_next_negative_sample[j] = n; // i.e., num_neg_samples * epochs_per_negative_sample;
             }
         }
     }
