8fabe938b9f1c931fb34cf35c8c8bef89a034c12
LTLA <LTLA@users.noreply.github.com>
2021-09-07 04:27:34 +0000

    Deploying to gh-pages from @ LTLA/umappp@719e3b1b2fdc8c26d59926a8df5de458e6f5e636 🚀

--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['initialize_0',['initialize',['../classumappp_1_1Umap.html#a612ca0310d865a625f50e75399b9f8a2',1,'umappp::Umap::initialize(NeighborList x, int ndim, double *embedding) const'],['../classumappp_1_1Umap.html#a5b0804f2bfbc3592951cdfdebdf819a6',1,'umappp::Umap::initialize(int ndim_in, size_t nobs, const Input *input, int ndim_out, double *embedding)'],['../classumappp_1_1Umap.html#a671a1292cf999e05d4f61427d207b2c2',1,'umappp::Umap::initialize(const Algorithm *searcher, int ndim, double *embedding)']]]
+  ['initialize_0',['initialize',['../classumappp_1_1Umap.html#a612ca0310d865a625f50e75399b9f8a2',1,'umappp::Umap::initialize(NeighborList x, int ndim, double *embedding) const'],['../classumappp_1_1Umap.html#ab70f346d3af52292a07e1b670c41e192',1,'umappp::Umap::initialize(int ndim_in, size_t nobs, const Input *input, int ndim_out, double *embedding)'],['../classumappp_1_1Umap.html#a6594321fa233a3da200d61c662e3221e',1,'umappp::Umap::initialize(const Algorithm *searcher, int ndim, double *embedding)']]]
 ];
