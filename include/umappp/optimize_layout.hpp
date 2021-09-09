3560743abbbeea81e1feb3befa9b4c27692ef3b0
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-09-09 00:05:31 -0700

    Initial draft of parallelized optimization with locks.

--- include/umappp/optimize_layout.hpp
+++ include/umappp/optimize_layout.hpp
@@ -9,6 +9,10 @@
 #include "NeighborList.hpp"
 #include "aarand/aarand.hpp"
 
+#ifdef _OPENMP
+#include "omp.h"
+#endif
+
 namespace umappp {
 
 struct EpochData {
@@ -175,6 +179,195 @@ inline void optimize_layout(
 
     return;
 }
+
+#ifdef _OPENMP
+struct Lock {
+    Lock() {
+        omp_init_lock(lock);
+    }
+    ~Lock() {
+        omp_destroy_lock(&lock);
+    }
+    void lock() {
+        omp_set_lock(&lock);
+    }
+    void unlock() {
+        omp_unset_lock(&lock);
+    }
+private:
+    omp_lock_t lock;
+};
+
+template<class Setup, class Rng>
+inline void optimize_layout_parallel(
+    int ndim,
+    double* embedding, 
+    Setup& setup,
+    double a, 
+    double b, 
+    double gamma,
+    double initial_alpha,
+    Rng& rng,
+    int epoch_limit
+) {
+    auto& n = setup.current_epoch;
+    auto num_epochs = setup.total_epochs;
+    auto limit_epochs = num_epochs;
+    if (limit_epochs > 0) {
+        limit_epochs = std::min(limit_epochs, num_epochs);
+    }
+
+    auto& head = setup.head;
+    auto& tail = setup.tail;
+    auto& epochs_per_sample = setup.epochs_per_sample;
+    auto& epoch_of_next_sample = setup.epoch_of_next_sample;
+    auto& epoch_of_next_negative_sample = setup.epoch_of_next_negative_sample;
+
+    const size_t num_obs = head.size(); 
+    constexpr double dist_eps = std::numeric_limits<double>::epsilon();
+    const double negative_sample_rate = setup.negative_sample_rate;
+
+    // Defining the gradient parameters.
+    constexpr double min_gradient = -4;
+    constexpr double max_gradient = 4;
+
+    Lock glock;
+    std::vector<Lock> olock(num_obs);
+
+    for (; n < limit_epochs; ++n) {
+        const double alpha = initial_alpha * (1.0 - static_cast<double>(n) / num_epochs);
+        size_t i_ = 0;
+
+        #pragma omp parallel
+        {
+            std::vector<std::pair<size_t, size_t> > tails;
+            std::vector<size_t> random_draws;
+            std::vector<int> encountered(num_obs);
+
+            while (1) {
+                size_t i;
+                {
+                    glock.lock(); 
+                    if (i_ == setup.head.size()) {
+                        glock.unlock();
+                        break;
+                    }
+
+                    i = i_;
+                    ++i_;
+
+                    size_t start = (i == 0 ? 0 : setup.head[i-1]), end = setup.head[i];
+
+                    olocks[i].lock();
+                    tails.clear();
+                    random_draws.clear();
+
+                    for (size_t j = start; j < end; ++j) {
+                        if (epoch_of_next_sample[j] > n) {
+                            continue;
+                        }
+
+                        auto other = setup.tail[j];
+                        if (!encountered[other]) {
+                            olocks[other].lock();
+                        }
+                        ++encountered[other];
+               
+                        const size_t num_neg_samples = (n - epoch_of_next_negative_sample[j]) * negative_sample_rate / epochs_per_sample[j];
+                        for (size_t p = 0; p < num_neg_samples; ++p) {
+                            size_t sampled = aarand::discrete_uniform(rng, num_obs); 
+                            if (sampled == i) {
+                                continue;
+                            }
+
+                            random_draws.push_back(sampled);
+                            if (!encountered[sampled]) {
+                                olocks[sampled].lock();
+                            }
+                            ++encountered[sampled];
+                        }
+
+                        tails.emplace_back(other, random_draws.size());
+
+                        // Sticking all epoch operations here for consistency.
+                        epoch_of_next_sample[j] += epochs_per_sample[j];
+                        epoch_of_next_negative_sample[j] = n;
+                    }
+                    glock.unlock(); 
+                }
+
+                double* left = embedding + i * ndim;
+                size_t previous_random = 0;
+
+                for (auto current : tails) {              
+                    double dist2 = 0;
+                    double* right = embedding + current.first * ndim;
+                    {
+                        const double* lcopy = left;
+                        const double* rcopy = right;
+                        for (int d = 0; d < ndim; ++d, ++lcopy, ++rcopy) {
+                            dist2 += (*lcopy - *rcopy) * (*lcopy - *rcopy);
+                        }
+                        dist2 = std::max(dist_eps, dist2);
+                    }
+
+                    const double pd2b = std::pow(dist2, b);
+                    const double grad_coef = (-2 * a * b * pd2b) / (dist2 * (a * pd2b + 1.0));
+                    {
+                        double* lcopy = left;
+                        double* rcopy = right;
+                        for (int d = 0; d < ndim; ++d, ++lcopy, ++rcopy) {
+                            double gradient = alpha * std::min(std::max(grad_coef * (*lcopy - *rcopy), min_gradient), max_gradient);
+                            *lcopy += gradient;
+                            *rcopy -= gradient;
+                        }
+                    }
+
+                    --encountered[current.first];
+                    if (!encountered[current.first]) {
+                        olocks[current.first].unlock();
+                    }
+
+                    while (previous_random < current.second) {
+                        size_t sampled = random_draws[previous_random];
+                        double dist2 = 0;
+                        double* right = embedding + sampled * ndim;
+                        {
+                            const double* lcopy = left;
+                            const double* rcopy = right;
+                            for (int d = 0; d < ndim; ++d, ++lcopy, ++rcopy) {
+                                dist2 += (*lcopy - *rcopy) * (*lcopy - *rcopy);
+                            }
+                            dist2 = std::max(dist_eps, dist2);
+                        }
+
+                        const double grad_coef = 2 * gamma * b / ((0.001 + dist2) * (a * std::pow(dist2, b) + 1.0));
+                        {
+                            double* lcopy = left;
+                            const double* rcopy = right;
+                            for (int d = 0; d < ndim; ++d, ++lcopy, ++rcopy) {
+                                *lcopy += alpha * std::min(std::max(grad_coef * (*lcopy - *rcopy), min_gradient), max_gradient);
+                            }
+                        }
+
+                        --encountered[sampled];
+                        if (encountered[sampled] == 0) {
+                            olocks[sampled].unlock();
+                        }
+
+                        ++previous_random;
+                    }
+                }
+
+                olocks[i].unlock();
+            }
+        }
+    }
+
+    return;
+}
+
+#endif
 
 }
 
