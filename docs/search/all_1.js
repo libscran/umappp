4358d9a83a02f1feed17e58625f7ad13503a5fac
LTLA <LTLA@users.noreply.github.com>
2021-09-12 00:16:18 +0000

    Deploying to gh-pages from @ LTLA/umappp@840ebd8245906c915dd5b4a302d228eb3298cf24 🚀

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['b_0',['b',['../structumappp_1_1Umap_1_1Defaults.html#a0d674254b0d60bf7c7af584ea2ded932',1,'umappp::Umap::Defaults']]],
-  ['bandwidth_1',['bandwidth',['../structumappp_1_1Umap_1_1Defaults.html#ac682e948e4ec69221a7bf183fbf888fd',1,'umappp::Umap::Defaults']]]
+  ['bandwidth_1',['bandwidth',['../structumappp_1_1Umap_1_1Defaults.html#ac682e948e4ec69221a7bf183fbf888fd',1,'umappp::Umap::Defaults']]],
+  ['batch_2',['batch',['../structumappp_1_1Umap_1_1Defaults.html#a8eb451bacc4dd65c97ab20e46bc45fa1',1,'umappp::Umap::Defaults']]]
 ];
