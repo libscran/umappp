39ac21412fd65d0a6c9317cd89f8e10ae525d8c2
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-09-25 16:19:33 -0700

    Added the umappp umbrella header.

---
+++ include/umappp/umappp.hpp
@@ -0,0 +1,13 @@
+#ifndef UMAPPP_UMAPPP_HPP
+#define UMAPPP_UMAPPP_HPP
+
+/**
+ * @file umappp.hpp
+ *
+ * @brief Overall header for the **umappp** library.
+ * This allows for a reasonably intuitive `#include "umappp/umappp.hpp"` call to include the library.
+ */
+
+#include "Umap.hpp"
+
+#endif
