b4c458ed08534c8e0bd83956606b90ddb13994a1
LTLA <LTLA@users.noreply.github.com>
2021-09-25 23:21:48 +0000

    Deploying to gh-pages from @ LTLA/umappp@39ac21412fd65d0a6c9317cd89f8e10ae525d8c2 🚀

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['umap_0',['Umap',['../classumappp_1_1Umap.html',1,'umappp']]],
-  ['umap_2ehpp_1',['Umap.hpp',['../Umap_8hpp.html',1,'']]]
+  ['umap_2ehpp_1',['Umap.hpp',['../Umap_8hpp.html',1,'']]],
+  ['umappp_2ehpp_2',['umappp.hpp',['../umappp_8hpp.html',1,'']]]
 ];
--- docs/search/files_1.js
+++ docs/search/files_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['umap_2ehpp_0',['Umap.hpp',['../Umap_8hpp.html',1,'']]]
+  ['umap_2ehpp_0',['Umap.hpp',['../Umap_8hpp.html',1,'']]],
+  ['umappp_2ehpp_1',['umappp.hpp',['../umappp_8hpp.html',1,'']]]
 ];
