3da665eee5f9477d4c7bcc3ecb8111e479594b86
LTLA <LTLA@users.noreply.github.com>
2021-12-12 23:41:52 +0000

    Deploying to gh-pages from @ LTLA/umappp@46e169b8df80725abca76e8007840ec17c54c819 🚀

--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -4,6 +4,7 @@ var searchData=
   ['neighbor_1',['Neighbor',['../NeighborList_8hpp.html#a276ec7786ac002c8cd3faa096c2de846',1,'umappp']]],
   ['neighborlist_2',['NeighborList',['../NeighborList_8hpp.html#aab5cd1cc7e9720f055ba2496f97d9172',1,'umappp']]],
   ['neighborlist_2ehpp_3',['NeighborList.hpp',['../NeighborList_8hpp.html',1,'']]],
-  ['num_5fepochs_4',['num_epochs',['../structumappp_1_1Umap_1_1Defaults.html#a2ce5003bbe402960498a29f3221f7bef',1,'umappp::Umap::Defaults::num_epochs()'],['../structumappp_1_1Umap_1_1Status.html#a3e039db8c5f77c931a7984f943b90a2b',1,'umappp::Umap::Status::num_epochs()']]],
-  ['num_5fneighbors_5',['num_neighbors',['../structumappp_1_1Umap_1_1Defaults.html#a3f69b649356b76ced4468c03be8330c7',1,'umappp::Umap::Defaults']]]
+  ['nobs_4',['nobs',['../structumappp_1_1Umap_1_1Status.html#a5c8c34ce62da51e6fea64d6bb955298b',1,'umappp::Umap::Status']]],
+  ['num_5fepochs_5',['num_epochs',['../structumappp_1_1Umap_1_1Defaults.html#a2ce5003bbe402960498a29f3221f7bef',1,'umappp::Umap::Defaults::num_epochs()'],['../structumappp_1_1Umap_1_1Status.html#a3e039db8c5f77c931a7984f943b90a2b',1,'umappp::Umap::Status::num_epochs()']]],
+  ['num_5fneighbors_6',['num_neighbors',['../structumappp_1_1Umap_1_1Defaults.html#a3f69b649356b76ced4468c03be8330c7',1,'umappp::Umap::Defaults']]]
 ];
--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['num_5fepochs_0',['num_epochs',['../structumappp_1_1Umap_1_1Status.html#a3e039db8c5f77c931a7984f943b90a2b',1,'umappp::Umap::Status']]]
+  ['nobs_0',['nobs',['../structumappp_1_1Umap_1_1Status.html#a5c8c34ce62da51e6fea64d6bb955298b',1,'umappp::Umap::Status']]],
+  ['num_5fepochs_1',['num_epochs',['../structumappp_1_1Umap_1_1Status.html#a3e039db8c5f77c931a7984f943b90a2b',1,'umappp::Umap::Status']]]
 ];
