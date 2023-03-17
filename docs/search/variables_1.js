019736f9c60004ff7b1e8dafd80cee76556d97bd
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-03-17 05:12:46 +0000

    Deploying to gh-pages from @ LTLA/umappp@d5790599c001accb5bbc85ec289b5a7039e24ec0 🚀

--- docs/search/variables_1.js
+++ docs/search/variables_1.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['b_0',['b',['../structumappp_1_1Umap_1_1Defaults.html#a7d478096697d7eebb22fd4db045adaee',1,'umappp::Umap::Defaults']]],
-  ['bandwidth_1',['bandwidth',['../structumappp_1_1Umap_1_1Defaults.html#a233c1244942cb75695cfe255052b496a',1,'umappp::Umap::Defaults']]],
-  ['batch_2',['batch',['../structumappp_1_1Umap_1_1Defaults.html#a7f4a3831946fd8dcad0ec7fd0909ec38',1,'umappp::Umap::Defaults']]]
+  ['bandwidth_1',['bandwidth',['../structumappp_1_1Umap_1_1Defaults.html#a233c1244942cb75695cfe255052b496a',1,'umappp::Umap::Defaults']]]
 ];
