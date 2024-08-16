04fd044da3e7d429f5b0fbe3e8c81320af7ca91d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-08-16 11:58:01 -0700

    Get bit closer.

--- tests/src/combine_neighbor_sets.cpp
+++ tests/src/combine_neighbor_sets.cpp
@@ -7,10 +7,12 @@
 #include <random>
 #include <cmath>
 
-class SetCombiningTest : public ::testing::TestWithParam<std::tuple<int, int> > {
+class CombineNeighborSetTest : public ::testing::TestWithParam<std::tuple<int, int> > {
 protected:
-    template<class Param>
-    auto generate_neighbors (Param p) { 
+    std::vector<std::vector<std::pair<int, double> > > neighbors;
+
+    void SetUp() { 
+        auto p = GetParam();
         size_t nobs = std::get<0>(p);
         int k = std::get<1>(p);
         int ndim = 5;
@@ -23,111 +25,112 @@ protected:
             data[r] = dist(rng);
         }
 
-        std::vector<std::vector<std::pair<int, double> > > stored;
-        stored.reserve(nobs);
+        neighbors.resize(nobs);
 
-        knncolle::VpTreeEuclidean<> searcher(ndim, nobs, data.data());
+        auto index = knncolle::VptreeBuilder().build_unique(knncolle::SimpleMatrix<int, int, double>(ndim, nobs, data.data()));
+        auto searcher = index->initialize();
+        std::vector<int> indices;
+        std::vector<double> distances;
         for (size_t i = 0; i < nobs; ++i) {
-            stored.push_back(searcher.find_nearest_neighbors(i, k));
-            for (auto& x : stored.back()) {
-                x.second = std::exp(-x.second);
+            searcher->search(i, k, &indices, &distances);
+
+            size_t actual_k = indices.size();
+            for (size_t x = 0; x < actual_k; ++x) {
+                neighbors[i].emplace_back(indices[x], std::exp(-distances[x]));
+            }
+        }
+    }
+
+protected:
+    template<class Searched_>
+    static void slow_symmetrization(const Searched_& original, const Searched_& combined, double mix_ratio = 1) {
+        std::map<std::pair<int, int>, double> probs;
+
+        // Filling 'probs'.
+        for (size_t i = 0; i < original.size(); ++i) {
+            const auto& y = original[i];
+            for (const auto& z : y) {
+                std::pair<int, int> target;
+                target.first = std::max(z.first, static_cast<int>(i));
+                target.second = std::min(z.first, static_cast<int>(i));
+
+                auto it = probs.find(target);
+                if (it != probs.end()) {
+                    it->second *= -1;
+                    if (mix_ratio == 1) {
+                        it->second += z.second - z.second * it->second;
+                    } else if (mix_ratio == 0) {
+                        it->second *= z.second;
+                    } else {
+                        double prod = it->second * z.second;
+                        it->second = mix_ratio * (z.second + it->second - prod) + (1 - mix_ratio) * prod;
+                    }
+                } else {
+                    probs[target] = -z.second;
+                }
             }
         }
 
-        return stored;
+        auto it = probs.begin();
+        while (it != probs.end()) {
+            auto& x = *it;
+            if (x.second < 0) {
+                if (mix_ratio == 0) {
+                    probs.erase(it++);
+                    continue;
+                }
+                
+                if (mix_ratio == 1) {
+                    x.second *= -1;
+                } else {
+                    x.second *= -mix_ratio;
+                }
+            }
+            ++it;
+        }
+
+        // Comparing to the combined results.
+        std::map<std::pair<int, int>, bool> found;
+        for (size_t i = 0; i < combined.size(); ++i) {
+            const auto& y = combined[i];
+            for (const auto& z : y) {
+                std::pair<int, int> target;
+                target.first = std::max(z.first, static_cast<int>(i));
+                target.second = std::min(z.first, static_cast<int>(i));
+
+                auto it = probs.find(target);
+                EXPECT_TRUE(it != probs.end());
+                if (it != probs.end()) {
+                    EXPECT_FLOAT_EQ(it->second, z.second);
+                }
+                found[target] = true;
+            }
+        }
+
+        EXPECT_EQ(probs.size(), found.size());
     }
 };
 
-template<class Searched>
-void slow_symmetrization(const Searched& original, const Searched& combined, double mix_ratio = 1) {
-    std::map<std::pair<int, int>, double> probs;
+TEST_P(CombineNeighborSetTest, Combining) {
+    auto Union = neighbors;
+    umappp::internal::combine_neighbor_sets<>(Union, 1.0);
+    slow_symmetrization(neighbors, Union, 1);
 
-    // Filling 'probs'.
-    for (size_t i = 0; i < original.size(); ++i) {
-        const auto& y = original[i];
-        for (const auto& z : y) {
-            std::pair<int, int> target;
-            target.first = std::max(z.first, static_cast<int>(i));
-            target.second = std::min(z.first, static_cast<int>(i));
+    auto intersect = neighbors;
+    umappp::internal::combine_neighbor_sets<>(intersect, 0.0);
+    slow_symmetrization(neighbors, intersect, 0);
 
-            auto it = probs.find(target);
-            if (it != probs.end()) {
-                it->second *= -1;
-                if (mix_ratio == 1) {
-                    it->second += z.second - z.second * it->second;
-                } else if (mix_ratio == 0) {
-                    it->second *= z.second;
-                } else {
-                    double prod = it->second * z.second;
-                    it->second = mix_ratio * (z.second + it->second - prod) + (1 - mix_ratio) * prod;
-                }
-            } else {
-                probs[target] = -z.second;
-            }
-        }
-    }
-
-    auto it = probs.begin();
-    while (it != probs.end()) {
-        auto& x = *it;
-        if (x.second < 0) {
-            if (mix_ratio == 0) {
-                probs.erase(it++);
-                continue;
-            }
-            
-            if (mix_ratio == 1) {
-                x.second *= -1;
-            } else {
-                x.second *= -mix_ratio;
-            }
-        }
-        ++it;
-    }
-
-    // Comparing to the combined results.
-    std::map<std::pair<int, int>, bool> found;
-    for (size_t i = 0; i < combined.size(); ++i) {
-        const auto& y = combined[i];
-        for (const auto& z : y) {
-            std::pair<int, int> target;
-            target.first = std::max(z.first, static_cast<int>(i));
-            target.second = std::min(z.first, static_cast<int>(i));
-
-            auto it = probs.find(target);
-            EXPECT_TRUE(it != probs.end());
-            if (it != probs.end()) {
-                EXPECT_FLOAT_EQ(it->second, z.second);
-            }
-            found[target] = true;
-        }
-    }
-
-    EXPECT_EQ(probs.size(), found.size());
-}
-
-TEST_P(SetCombiningTest, Combining) {
-    auto stored = generate_neighbors(GetParam());
-
-    auto Union = stored;
-    umappp::combine_neighbor_sets<>(Union, 1.0);
-    slow_symmetrization(stored, Union, 1);
-
-    auto intersect = stored;
-    umappp::combine_neighbor_sets<>(intersect, 0.0);
-    slow_symmetrization(stored, intersect, 0);
-
-    auto middle = stored;
-    umappp::combine_neighbor_sets<>(middle, 0.5);
-    slow_symmetrization(stored, middle, 0.5);
+    auto middle = neighbors;
+    umappp::internal::combine_neighbor_sets<>(middle, 0.5);
+    slow_symmetrization(neighbors, middle, 0.5);
 
     // Comparing the number of edges.
     size_t total_u = 0, total_i = 0, total_o = 0;
-    for (size_t i = 0; i < stored.size(); ++i) {
+    for (size_t i = 0; i < neighbors.size(); ++i) {
         const auto& uvec = Union[i];
         const auto& ivec = intersect[i];
         const auto& mvec = middle[i];
-        const auto& orig = stored[i];
+        const auto& orig = neighbors[i];
         
         EXPECT_TRUE(uvec.size() == mvec.size());
         EXPECT_TRUE(uvec.size() >= ivec.size());
@@ -145,8 +148,8 @@ TEST_P(SetCombiningTest, Combining) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    SetCombining,
-    SetCombiningTest,
+    CombineNeighborSet,
+    CombineNeighborSetTest,
     ::testing::Combine(
         ::testing::Values(50, 100, 200), // number of observations
         ::testing::Values(5, 10, 15) // number of neighbors 
