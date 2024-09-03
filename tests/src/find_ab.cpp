5e7ee2547b45b5a6996fc568e308923d609a6b84
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-09-03 00:00:45 -0700

    Actually use Gauss-Newton + Levenberg-Marquardt dampening in the AB search.
    This greatly simplifies the calculations as we only need the first derivative
    of the residual functions, not the first + second derivatives of the squares.

--- tests/src/find_ab.cpp
+++ tests/src/find_ab.cpp
@@ -6,14 +6,33 @@
 TEST(ABTest, FindAB) {
     // Comparing to values from uwot:::find_ab_params()
     auto out = umappp::internal::find_ab<double>(1, 0.05);
-    EXPECT_TRUE(std::abs(out.first - 1.7502250) < 1e-3);
-    EXPECT_TRUE(std::abs(out.second - 0.8420554) < 1e-3);
+    EXPECT_LT(std::abs(out.first - 1.7502250), 1e-4);
+    EXPECT_LT(std::abs(out.second - 0.8420554), 1e-4);
 
     out = umappp::internal::find_ab<double>(1, 0.01);
-    EXPECT_TRUE(std::abs(out.first - 1.8956059) < 1e-3);
-    EXPECT_TRUE(std::abs(out.second - 0.800637) < 1e-3);
+    EXPECT_LT(std::abs(out.first - 1.8956059), 1e-4);
+    EXPECT_LT(std::abs(out.second - 0.800637), 1e-4);
 
     out = umappp::internal::find_ab<double>(2, 0.1);
-    EXPECT_TRUE(std::abs(out.first - 0.5446606) < 1e-3);
-    EXPECT_TRUE(std::abs(out.second - 0.8420553) < 1e-3);
+    EXPECT_LT(std::abs(out.first - 0.5446606), 1e-4);
+    EXPECT_LT(std::abs(out.second - 0.8420553), 1e-4);
+}
+
+TEST(ABTest, Dampening) {
+    // Testing the dampening. This doesn't work by default with uwot, we need to fiddle with the parameters.
+    // > spread <- 200
+    // > min_dist <- 1
+    // > xv <- seq(from = 0, to = spread * 3, length.out = 300)
+    // > yv <- rep(0, length(xv))
+    // > yv[xv < min_dist] <- 1
+    // > yv[xv >= min_dist] <- exp(-(xv[xv >= min_dist] - min_dist)/spread)
+    // > stats::nls(yv ~ 1/(1 + a * xv^(2 * b)), start = list(a = 0.000417953, b = 0.79548))$m$getPars()
+    auto out = umappp::internal::find_ab<double>(200, 1);
+    EXPECT_LT(std::abs(out.first - 0.0004176367), 1e-6);
+    EXPECT_LT(std::abs(out.second - 0.7955526861), 1e-4);
+
+    // Getting some code coverage for dampening failure.
+    // This is achieved by forcing underflow of 'a'.
+    out = umappp::internal::find_ab<double>(20, 1000);
+    EXPECT_LT(out.first, 1e-100);
 }
