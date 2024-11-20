367cdca78460f5070ba82b77d24f0e509185fe80
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-11-20 01:08:35 +0000

    Deploying to gh-pages from @ libscran/umappp@a08ac553f5e3f9adf925fec9af0e8d172d8d5399 🚀

--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['can_5fsearch_5fall_0',['can_search_all',['https://knncolle.github.io/knncolle/classknncolle_1_1BruteforceSearcher.html#a9c7cd8e9f01f5798d5b98a1d5318e8f4',1,'knncolle::BruteforceSearcher::can_search_all()'],['https://knncolle.github.io/knncolle/classknncolle_1_1KmknnSearcher.html#a8fae155ea1722024e5dde21550674116',1,'knncolle::KmknnSearcher::can_search_all()'],['https://knncolle.github.io/knncolle/classknncolle_1_1Searcher.html#a8362d0f6ea1b7f6e09d26a9188d31d1d',1,'knncolle::Searcher::can_search_all()'],['https://knncolle.github.io/knncolle/classknncolle_1_1VptreeSearcher.html#ac7a0f11645ab4f57b9991602339e0e15',1,'knncolle::VptreeSearcher::can_search_all()']]],
-  ['create_5fworkspace_1',['create_workspace',['https://knncolle.github.io/knncolle/classknncolle_1_1MockMatrix.html#ab67d03fc96f29ca6d37fb00a1d7fddc3',1,'knncolle::MockMatrix']]]
+  ['cap_5fk_1',['cap_k',['https://knncolle.github.io/knncolle/namespaceknncolle.html#a926c81cd626de7081b073a891d352543',1,'knncolle']]],
+  ['create_5fworkspace_2',['create_workspace',['https://knncolle.github.io/knncolle/classknncolle_1_1MockMatrix.html#ab67d03fc96f29ca6d37fb00a1d7fddc3',1,'knncolle::MockMatrix']]]
 ];
