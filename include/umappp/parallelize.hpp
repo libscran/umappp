2df8884a57079a9126ff7593befbb0838d4b457a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-02-27 12:41:00 +1100

    Updated parallelize() override to return number of used workers.
    This is consistent with the updated subpar::parallelize_range() function. Also
    fleshed out the docs to better describe why and how to override it.
    
    Also updated the versions of dependencies and bumped version for new release.

--- include/umappp/parallelize.hpp
+++ include/umappp/parallelize.hpp
@@ -17,21 +17,28 @@ namespace umappp {
  * @tparam Run_ Function to execute a range of tasks.
  *
  * @param num_workers Number of workers.
+ * This should be positive.
  * @param num_tasks Number of tasks.
- * @param run_task_range Function to iterate over a range of tasks within a worker.
+ * This should be non-negative.
+ * @param run_task_range Function to iterate over a range of tasks within a worker,
+ * see the argument of the same name in `subpar::parallelize_range()`.
  *
- * By default, this is an alias to `subpar::parallelize_range()`.
- * However, if the `UMAPPP_CUSTOM_PARALLEL` function-like macro is defined, it is called instead. 
- * Any user-defined macro should accept the same arguments as `subpar::parallelize_range()`.
+ * return Number of used workers.
+ * This will be no greater than `num_workers`.
+ *
+ * By default, this function is an alias to `subpar::parallelize_range()`.
+ * Its purpose is to enable **umappp**-specific customization to the parallization scheme without affecting other libraries that use **subpar**.
+ * If the `UMAPPP_CUSTOM_PARALLEL` macro is defined, it will be used instead of `subpar::parallelize_range()` whenever `parallelize()` is called. 
+ * Any user-defined macro should follow the same requirements as the `SUBPAR_CUSTOM_PARALLELIZE_RANGE` override for `subpar::parallelize_range()`.
  */
 template<typename Task_, class Run_>
-void parallelize(const int num_workers, const Task_ num_tasks, Run_ run_task_range) {
+int parallelize(const int num_workers, const Task_ num_tasks, Run_ run_task_range) {
 #ifndef UMAPPP_CUSTOM_PARALLEL
     // Don't make this nothrow_ = true, there's too many allocations and the
     // derived methods for the nearest neighbors search could do anything...
-    subpar::parallelize(num_workers, num_tasks, std::move(run_task_range));
+    return subpar::parallelize_range(num_workers, num_tasks, std::move(run_task_range));
 #else
-    UMAPPP_CUSTOM_PARALLEL(num_workers, num_tasks, run_task_range);
+    return UMAPPP_CUSTOM_PARALLEL(num_workers, num_tasks, run_task_range);
 #endif
 }
 
--- tests/src/custom_parallel.h
+++ tests/src/custom_parallel.h
@@ -1,26 +1,7 @@
 #ifndef CUSTOM_PARALLEL_H
 #define CUSTOM_PARALLEL_H
 
-#include <vector>
-#include <thread>
+#include "subpar/subpar.hpp"
 
-template<class Function_>
-void default_parallelize(size_t nthreads, size_t n, Function_ f) {
-    size_t jobs_per_worker = (n / nthreads) + (n % nthreads > 0);
-    size_t start = 0;
-    std::vector<std::thread> jobs;
-    jobs.reserve(nthreads);
-    
-    for (size_t w = 0; w < nthreads && start < n; ++w) {
-        size_t len = std::min(n - start, jobs_per_worker);
-        jobs.emplace_back(f, w, start, len);
-        start += len;
-    }
-
-    for (auto& job : jobs) {
-        job.join();
-    }
-}
-
-#define UMAPPP_CUSTOM_PARALLEL default_parallelize
+#define UMAPPP_CUSTOM_PARALLEL ::subpar::parallelize_range
 #endif
